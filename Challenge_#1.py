5f409c63380e9bb904c15e029d47bf420031070c
mccoymike85 <mccoymike85@gmail.com>
2018-06-04 11:44:11 -0400

    Add files via upload

---
+++ Challenge_#1.py
@@ -0,0 +1,26 @@
+#Function Definition
+def list_sum_k(num_list, k):
+    result = 'No'
+    length = num_list.__len__()
+    length = length - 1
+
+    x = 0
+    while x < length:
+        y = x + 1
+        print( 'x = ' + str(x) + '   y = ' + str(y))
+        while y <= length:
+            print('Checking ' + str(num_list[x]) + str(' and ') + str(num_list[y]))
+            if (num_list[x] + num_list[y] == k):
+                result = 'Yes'
+            y = y + 1
+        x = x + 1
+        y = x
+        print(' ')
+    print(result)
+
+
+
+#Driver
+num_list = [1,2,5,1,10,11,14,7]
+k = 6
+list_sum_k(num_list, k)
